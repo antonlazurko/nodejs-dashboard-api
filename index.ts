4f927ada52f2e000aeae41487cfcb1dc781066be
Anton Lazurko <antla@ciklum.com>
2024-11-13 11:51:35 +0200

    feat: project ts configure

--- index.ts
+++ index.ts
@@ -1,22 +1,22 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 import { usersRouter } from "./users/users.js";
 
 const port = 8000;
 
 const app = express();
 
-app.get('/hello', (req, res) => {
+app.get('/hello', (req: Request, res: Response) => {
     throw new Error('Error')
 })
 
-app.use((req, res, next) => {
+app.use((req: Request, res: Response, next: NextFunction) => {
     console.log('Time: ', Date.now());
     next();
 })
 
 app.use('/users', usersRouter)
 
-app.use((err, req, res, next) => {
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     console.error(err.message);
     res.status(500).send(err.message);
     // next();
