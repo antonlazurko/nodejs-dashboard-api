f6e70d79b38b4d66a5a63e2011a9b94a2b2e7819
Anton Lazurko <antla@ciklum.com>
2024-11-13 13:41:06 +0200

    feat: ts classes

--- test.ts
+++ test.ts
@@ -1,31 +1,98 @@
-interface IHasLength {
-    length: number
-}
-
-function log<T extends IHasLength, K>(obj:T, arr: K[]): K[] {
-    console.log(obj);
-    arr.length
-    obj.length
-    return arr
-}
-
-log<string, number>('hello', [1, 2, 3])
-log<Array<number>, string>([1], ['a', 'b', 'c'])
-
-function add<T>(value:T): T[] {
-    const arr: T[] = []
-    for (let i = 0; i < 10; i++) {
-        arr.push(value)
+class Coord {
+    message: string = "hello";
+    lat: number;
+    lng: number;
+    protected test() {// do not use in instance
+        if (this.lat === 0 && this.lng === 0) {
+            throw new Error("0, 0");
+        }
     }
-    return arr
+    private test2() {} // do not use in inherited class and instance only in derived
+    constructor(lat: number, lng: number) {
+        this.lat = lat;
+        this.lng = lng;
+        console.log(this.message);
+    }
+    computeDistance(newLat: number, newLng: number): number {
+        return 0;
+    }
+}
+const point = new Coord(0, 0);
+
+class MapLocation extends Coord {
+    message: string = "hello world";
+    private _name: string;
+    get name(): string {
+        return this._name;
+    }
+    set name(value: string) {
+        this._name = value;
+    }
+    constructor(lat: number, lng: number) {
+        super(lat, lng);
+    }
+    override computeDistance(newLat: number, newLng: number): number {
+        console.log(this.message);
+        return 0;
+        this.test();
+    }
+}
+console.log(new MapLocation(0, 0).computeDistance(0, 0));
+
+interface LoggerService {
+    log: (s: string) => void
 }
 
-interface IUser{
-    name : string
-    age?: number
-    bid: <T>(sum: T) => boolean
+class Logger implements LoggerService {
+    public log(s: string) {
+        console.log(s);
+    }
+    private error(s: string) {
+        console.error(s);
+    }
 }
 
-function bid<T>(params: T): boolean {
-    return true
+const log = new Logger();
+
+
+class MyClass {
+    static value: number = 0;
+    static getValue() {
+        return this.value;
+    }
 }
+console.log(MyClass.getValue());
+
+class NewClass <T> {
+    a: T
+}
+
+const b = new NewClass<number>();
+console.log(b.a);
+
+
+abstract class Base {
+    print(s: string) {
+        console.log(s);
+    }
+    abstract error(s: string): void
+}
+// new Base()// error
+
+class Derived extends Base {
+    error(s: string): void {
+        console.error(s);
+    }
+}
+const d = new Derived();
+d.print("hello");
+
+class Animal {
+    name: string
+}
+class Dog {
+    name: string
+    tail: boolean
+}
+
+const puppy: Animal = new Dog();
