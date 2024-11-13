86530037bea8ff4582b9d1953b838f9fd3f153ee
Anton Lazurko <antla@ciklum.com>
2024-11-13 11:04:43 +0200

    feat: router

--- index.js
+++ index.js
@@ -1,42 +1,11 @@
 import express from "express";
+import { usersRouter } from "./users/users.js";
 
 const port = 8000;
 
 const app = express();
 
-app.all("/hello", (req, res, next) => {
-    console.log('all :>> ');
-    next();
-})
-
-app.route('/').get((req, res) => {
-    // res.redirect(301, '/hello');
-    res.status(404).end()
-})
-app.route('/hello').get((req, res) => {
-        // res.set('Content-Type', 'text/plain');
-        res.type('application/json');
-        res.cookie('token', '12345', {
-            domain: 'localhost',
-            path: '/',
-            secure: true,
-            expires: new Date(0)
-        });
-        res.clearCookie('token', {
-            path: '/'
-        });
-        res.send("Hello World");
-        // res.download('./README.md', 'test.md');
-    })
-    .post((req, res) => {
-        res.send("Hello World post");
-    })
-    .put((req, res) => {
-        res.send("Hello World put");
-    })
-    .delete((req, res) => {
-        res.send("Hello World delete");
-    });
+app.use('/users', usersRouter)
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
---
+++ users/users.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const usersRouter = express.Router();
+
+usersRouter.post("/login", (req, res) => {
+    res.send("Login");
+});
+usersRouter.post("/register", (req, res) => {
+    res.send("Register");
+});
+
+export { usersRouter }
