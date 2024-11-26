7d09c0d3d12c1bb7718fc2944f75e77341a6c47e
Anton Lazurko <antla@ciklum.com>
2024-11-26 18:53:58 +0200

    handler

---
+++ netlify/functions/main.ts
@@ -0,0 +1,9 @@
+// netlify/functions/main.ts
+import { Handler } from '@netlify/functions';
+
+export const handler: Handler = async (event, context) => {
+	return {
+		statusCode: 200,
+		body: JSON.stringify({ message: 'Hello from Netlify!' }),
+	};
+};
